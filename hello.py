901a3dd2dac043452200aa5b74af3a51930c7be9
hky404 <harshkumaryadavg@gmail.com>
2017-11-18 23:19:53 -0800

    added some routes

--- hello.py
+++ hello.py
@@ -2,8 +2,23 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+	return 'Index Page'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+	# show the user profile for that user
+	return "User {}".format(username)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+	# show the post with the given id, the id must be an integer
+	return "Post {}".format(post_id)
+
+@app.route('/hello')
 def hello_world():
-	return 'Hello World!!!!!'
+	return 'Hello Everyone!!'
 
 if __name__ == '__main__':
+	app.debug = True
 	app.run()
