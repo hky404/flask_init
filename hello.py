4b02147639533d428b3315bc8fe86c5930f73886
hky404 <harshkumaryadavg@gmail.com>
2017-12-27 21:07:22 -0800

    added logo and logout functionality

--- hello.py
+++ hello.py
@@ -1,28 +1,43 @@
-from flask import Flask, request, render_template, redirect, url_for, flash
+from flask import Flask, request, render_template, redirect, url_for, flash, make_response
 
 app = Flask(__name__)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	error = None
-	if request.method == 'POST':
-		if valid_login(request.form['username'], request.form['password']):
-			flash("Succesfully logged in")
-			return redirect(url_for('welcome', username=request.form['username']))
-		else:
-			error = 'Incorrect username and password'
-	return render_template('login.html', error_var=error)
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'], request.form['password']):
+            flash("Succesfully logged in")
+            response = make_response(redirect(url_for('welcome')))
+            response.set_cookie('username', request.form.get('username'))
+            return response
+        else:
+            error = 'Incorrect username and password'
+    return render_template('login.html', error_var=error)
+
+
+@app.route('/logout')
+def logout():
+    response = make_response(redirect(url_for('login')))
+    response.set_cookie('username', '', expires=0)
+    return response
+
 
 def valid_login(username, password):
-	if username == password:
-		return True
-	else:
-		return False
+    if username == password:
+        return True
+    else:
+        return False
 
-@app.route('/welcome/<username>')
-def welcome(username):
-	return render_template('welcome.html', username_var=username)
+
+@app.route('/')
+def welcome():
+    username = request.cookies.get('username')
+    if username:
+        return render_template('welcome.html', username_var=username)
+    else:
+        return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
