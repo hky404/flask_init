1397d9c4d716e09639f0bf9684664414085c4c17
hky404 <harshkumaryadavg@gmail.com>
2017-12-28 18:38:13 -0800

    initated logging

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
-from flask import Flask, request, render_template, redirect, url_for, flash, make_response
+from flask import Flask, request, render_template, redirect, url_for, flash, session
 
 app = Flask(__name__)
+
+import logging
+from logging.handlers import RotatingFileHandler
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -9,19 +12,18 @@ def login():
     if request.method == 'POST':
         if valid_login(request.form['username'], request.form['password']):
             flash("Succesfully logged in")
-            response = make_response(redirect(url_for('welcome')))
-            response.set_cookie('username', request.form.get('username'))
-            return response
+            session['username'] = request.form['username']
+            return redirect(url_for('welcome'))
         else:
             error = 'Incorrect username and password'
+            app.logger.warning("Incorrect username and password for user ({})".format(request.form.get('username')))
     return render_template('login.html', error_var=error)
 
 
 @app.route('/logout')
 def logout():
-    response = make_response(redirect(url_for('login')))
-    response.set_cookie('username', '', expires=0)
-    return response
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 
 def valid_login(username, password):
@@ -33,14 +35,19 @@ def valid_login(username, password):
 
 @app.route('/')
 def welcome():
-    username = request.cookies.get('username')
-    if username:
-        return render_template('welcome.html', username_var=username)
+    if 'username' in session:
+        return render_template('welcome.html', username_var=session['username'])
     else:
         return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
     app.debug = True
-    app.secret_key = 'SuperSecretKeyxyz'
+    app.secret_key = '\xa1!Y\xd3\xe3\xf2\x1c\xad\xa8\x1ad\xb1U\xb11k\xce\xc9\xe0t\xde\xfe\x9f\x9d'
+
+    # logging
+    handler = RotatingFileHandler('error.log', maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.INFO)
+    app.logger.addHandler(handler)
+    
     app.run()
